5c0ee535bdbd50359875c84df001ebe2dad8edab
berkctezc <berkcantezcaner@gmail.com>
2021-03-31 18:32:35 +0300

    business,da, entity layers done

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _carList;
+        public InMemoryCarDal()
+        {
+            _carList = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=150000,ModelYear="2002",Description="Külüstür"},
+                new Car{Id=2,BrandId=2,ColorId=2,DailyPrice=800000,ModelYear="1968",Description="Retro"},
+                new Car{Id=3,BrandId=2,ColorId=3,DailyPrice=250000,ModelYear="1996",Description="Aile"},
+                new Car{Id=3,BrandId=2,ColorId=1,DailyPrice=300000,ModelYear="2012",Description="Spor"},
+                new Car{Id=4,BrandId=3,ColorId=2,DailyPrice=80000,ModelYear="2008",Description="Günlük"},
+                new Car{Id=5,BrandId=3,ColorId=3,DailyPrice=130000,ModelYear="2018",Description="Fiyat-Performans"},
+                new Car{Id=6,BrandId=4,ColorId=1,DailyPrice=260000,ModelYear="2015",Description="Dağ"},
+                new Car{Id=7,BrandId=5,ColorId=3,DailyPrice=110000,ModelYear="2005",Description="Külüstür v2"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _carList.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDel = null;
+            carToDel = _carList.SingleOrDefault(c => c.Id == c.Id);
+
+            _carList.Remove(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carList;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _carList.Where(c=>c.BrandId==brandId).ToList();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _carList.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate=_carList.SingleOrDefault(c=>c.Id==car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
