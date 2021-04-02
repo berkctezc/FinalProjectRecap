ef0f7c6ad4183d98917ac3e03ed8331c13d6614d
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-02 14:55:09 +0300

    db creation query

---
+++ CarProjectDB_Query.sql
@@ -0,0 +1,24 @@
+﻿--Database Creation
+create database CarProjectDB;
+
+--Car Table
+create table Cars(
+    Id int PRIMARY KEY not null,
+    BrandId int,
+    ColorId int,
+    ModelYear varchar(4),
+    DailyPrice int,
+    Description varchar(100)
+);
+
+--Brand Table
+create table Brands(
+       Id int PRIMARY KEY not null,
+       Name varchar(100)
+);
+
+--Color table
+create table Colors(
+       Id int PRIMARY KEY not null,
+       Name varchar(100)
+);
